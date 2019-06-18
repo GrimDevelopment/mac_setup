96aa285596db5cca4d956f59dea49d6ea4524e84
Jeremiah Nofrada <jnofrada@thevaliantway.com>
2019-06-18 18:36:25 -0400

    Mac Rename edits

--- MacRename.py
+++ MacRename.py
@@ -1,7 +1,18 @@
 import subprocess
 
-#Renames computer name, host name, and local host name
-name = raw_input("What is the computer name: ")
-subprocess.call(["sudo", "scutil", "--set", "LocalHostName", name])
-subprocess.call(["sudo", "scutil", "--set", "HostName", name])
-subprocess.call(["sudo", "scutil", "--set", "ComputerName", name])
+#Opens computer name text ile
+comp = open("comp_names.txt")
+
+#Gathers current computer name
+current_name = subprocess.call(["scutil", "--get", "computerName"])
+
+#Finds current computer name in text file and compares to current computer name
+for x in range(len(clients_list)):
+    line = str(clients.readline())
+    if not line: break
+
+
+    #Renames computer name, host name, and local host name
+    subprocess.call(["sudo", "scutil", "--set", "LocalHostName", name])
+    subprocess.call(["sudo", "scutil", "--set", "HostName", name])
+    subprocess.call(["sudo", "scutil", "--set", "ComputerName", name])
