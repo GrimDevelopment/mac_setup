796f16340810dd8c281e0f6a3669f6975fadd147
Jeremiah Nofrada <jnofrada@thevaliantway.com>
2019-06-18 18:41:14 -0400

    Mac Rename edits

--- MacRename.py
+++ MacRename.py
@@ -7,12 +7,14 @@ comp = open("comp_names.txt")
 current_name = subprocess.call(["scutil", "--get", "computerName"])
 
 #Finds current computer name in text file and compares to current computer name
-for x in range(len(clients_list)):
-    line = str(clients.readline())
+for line in comp:
     if not line: break
+    field = line.split(",")
+    name1 = field[0]
+    name2 = field[1]
 
-
-    #Renames computer name, host name, and local host name
-    subprocess.call(["sudo", "scutil", "--set", "LocalHostName", name])
-    subprocess.call(["sudo", "scutil", "--set", "HostName", name])
-    subprocess.call(["sudo", "scutil", "--set", "ComputerName", name])
+    if (current_name == name1):
+        #Renames computer name, host name, and local host name
+        subprocess.call(["sudo", "scutil", "--set", "LocalHostName", name])
+        subprocess.call(["sudo", "scutil", "--set", "HostName", name])
+        subprocess.call(["sudo", "scutil", "--set", "ComputerName", name])
