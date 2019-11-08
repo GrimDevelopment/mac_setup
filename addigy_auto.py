15b03a213e11cd1946632b3d532c45a66878454c
Jeremiah Nofrada <jnnofrada@gmail.com>
2019-11-08 17:57:13 -0500

    selenium automation

--- addigy_auto.py
+++ addigy_auto.py
@@ -1,6 +1,7 @@
+import subprocess
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-
+subprocess.call(["safaridriver", "--enable"])
 safari = webdriver.Safari()
 
 safari.get("http://prod.addigy.com")
